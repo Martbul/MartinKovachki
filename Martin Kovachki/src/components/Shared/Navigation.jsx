0ec07063442a1990fbb8c49861a96151072e12c6
Martbul <martbul01@gmail.com>
2024-04-19 23:34:21 +0300

    add navigation

--- Martin Kovachki/src/components/Shared/Navigation.jsx
+++ Martin Kovachki/src/components/Shared/Navigation.jsx
@@ -1,11 +1,38 @@
+import "./Navigation.css"
 export default function Navigation() {
   return (
     <>
       <header>
-        <nav>
-            ul
+        <div className="site-title">
+        
+          <a href="#">Portfolio</a>
+        </div>
+        <nav className="navigation">
+          <ul>
+            <li className="nav-item">
+              <a to="/">Home</a>
+            </li>
+
+            <li className="nav-item">
+              {" "}
+              <a to="/post-app/create-app">Contacts</a>
+            </li>
+            
+            <li className="nav-item">
+              <a to="/user/profile">Projects</a>
+            </li>
 
             
+
+           
+            <li className="nav-item">
+              <a to="/about">About</a>
+            </li>
+
+            <li className="nav-item">
+              <a to="/user/login">More Info</a>
+            </li>
+          </ul>
         </nav>
       </header>
     </>

dbc61777182103f599a661474dca383bf3521970
Martbul <martbul01@gmail.com>
2024-04-22 22:22:16 +0300

    add docs

--- Martin Kovachki/src/Shared/Projects/Projects.jsx
+++ Martin Kovachki/src/Shared/Projects/Projects.jsx
@@ -15,7 +15,8 @@ export default function Projects() {
               </a>
             </h5>
             <p>
-           New app store. Web app where people can share their apps. You can develop, share and buy applications.
+              New app store. Web app where people can share their apps. You can
+              develop, share and buy applications.
             </p>
           </div>
         </div>
@@ -24,7 +25,7 @@ export default function Projects() {
           <div className="title"></div>
           <div className="content">
             <h5>
-              <a href="https://martbul.github.io/Alyst2.0/">Alyst2.0</a>
+              <a href="https://martbul.github.io/DevelopIt/">DevelopIt</a>
             </h5>
             <p>
               Modern website desing for a sotware agency that builds AI
@@ -39,7 +40,8 @@ export default function Projects() {
               <a href="https://martbul.github.io/pearl.bg/">PearlBg</a>
             </h5>
             <p>
-              Web app for second hand jewery. Everyone is welcome to sell and buy.
+              Web app for second hand jewery. Everyone is welcome to sell and
+              buy.
             </p>
           </div>
         </div>
@@ -50,7 +52,8 @@ export default function Projects() {
               <a href="https://martbul.github.io/ALYST/">Alyst AI</a>
             </h5>
             <p>
-              Basic site for service base business. Showing what services they offer, pricing and demos.
+              Basic site for service base business. Showing what services they
+              offer, pricing and demos.
             </p>
           </div>
         </div>
@@ -70,22 +73,28 @@ export default function Projects() {
         <div className="project">
           <div className="content">
             <h5>
-              <a href="https://github.com/Martbul/NodeJs-Express-Server">NodeJs-Express Server</a>
+              <a href="https://github.com/Martbul/NodeJs-Express-Server">
+                NodeJs-Express Server
+              </a>
             </h5>
             <p>
-              Fully developed, high-quality backend with NodeJs, Express and MongoDB. Can be used as templete for structuring other webapps.
+              Fully developed, high-quality backend with NodeJs, Express and
+              MongoDB. Can be used as templete for structuring other webapps.
             </p>
           </div>
         </div>
 
-
         <div className="project">
           <div className="content">
             <h5>
-              <a href="https://github.com/Martbul/FaceDetector">AI Face Detector</a>
+              <a href="https://github.com/Martbul/FaceDetector">
+                AI Face Detector
+              </a>
             </h5>
             <p>
-            AI model for face detection. It connects to the camera of your phone and if spots a face in the frame it draws a square around it.
+              AI model for face detection. It connects to the camera of your
+              phone and if spots a face in the frame it draws a square around
+              it.
             </p>
           </div>
         </div>

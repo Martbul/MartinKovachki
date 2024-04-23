b06f3b2cb88666869c0eaa554bf3ee270b3b925c
Martbul <martbul01@gmail.com>
2024-04-23 11:21:10 +0300

    add project and git reppos

--- Martin Kovachki/src/Shared/Projects/Projects.jsx
+++ Martin Kovachki/src/Shared/Projects/Projects.jsx
@@ -10,10 +10,31 @@ export default function Projects() {
         <div className="project">
           <div className="content">
             <h5>
+              <a href="https://martbul.github.io/optiflow/">Optiflow</a>
+            </h5>
+            <h6>
+              GitHub reppo:{" "}
+              <a href="https://github.com/Martbul/optiflow">
+                https://github.com/Martbul/optiflow
+              </a>
+            </h6>
+            <p>Website for an AI automation agency</p>
+          </div>
+        </div>
+
+        <div className="project">
+          <div className="content">
+            <h5>
               <a href="https://github.com/Martbul/angular-final-project">
                 SoftUni Final Project
               </a>
             </h5>
+            <h6>
+              GitHub reppo:{" "}
+              <a href="https://github.com/Martbul/angular-final-project">
+                https://github.com/Martbul/angular-final-project
+              </a>
+            </h6>
             <p>
               New app store. Web app where people can share their apps. You can
               develop, share and buy applications.
@@ -27,6 +48,12 @@ export default function Projects() {
             <h5>
               <a href="https://martbul.github.io/DevelopIt/">DevelopIt</a>
             </h5>
+            <h6>
+              GitHub reppo:{" "}
+              <a href="https://github.com/Martbul/DevelopIt">
+                https://github.com/Martbul/DevelopIt
+              </a>
+            </h6>
             <p>
               Modern website desing for a sotware agency that builds AI
               solutions
@@ -39,6 +66,12 @@ export default function Projects() {
             <h5>
               <a href="https://martbul.github.io/pearl.bg/">PearlBg</a>
             </h5>
+            <h6>
+              GitHub reppo:{" "}
+              <a href="https://github.com/Martbul/pearl.bg">
+                https://github.com/Martbul/pearl.bg
+              </a>
+            </h6>
             <p>
               Web app for second hand jewery. Everyone is welcome to sell and
               buy.
@@ -51,6 +84,12 @@ export default function Projects() {
             <h5>
               <a href="https://martbul.github.io/ALYST/">Alyst AI</a>
             </h5>
+            <h6>
+              GitHub reppo:{" "}
+              <a href="https://github.com/Martbul/ALYST">
+                https://github.com/Martbul/ALYST
+              </a>
+            </h6>
             <p>
               Basic site for service base business. Showing what services they
               offer, pricing and demos.
@@ -63,6 +102,12 @@ export default function Projects() {
             <h5>
               <a href="https://martbul.github.io/Bind/">Bind Delivery</a>
             </h5>
+            <h6>
+              GitHub reppo:{" "}
+              <a href="https://github.com/Martbul/Bind">
+                https://github.com/Martbul/Bind
+              </a>
+            </h6>
             <p>
               Web app with the idea of Uber, but for groceries - people
               deliverying groceries.
@@ -103,13 +148,13 @@ export default function Projects() {
           <div className="content">
             <h5>
               <a href="https://github.com/Martbul/CommentToxicity-Tracker">
-               AI Comment Toxicity Tracker
+                AI Comment Toxicity Tracker
               </a>
             </h5>
             <p>
-              This is an AI model that can evaluate emotions expressed ni comments. The AI can determine if 
-              someone's comment is agressive or hateful or positive or some other strong emotion
-              it.
+              This is an AI model that can evaluate emotions expressed ni
+              comments. The AI can determine if someone's comment is agressive
+              or hateful or positive or some other strong emotion it.
             </p>
           </div>
         </div>
